d6cfcb5bbf8e776ed1b84b36d2ed6779f809b63b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2024-08-25 16:33:27 +0200

    link terarkdb

--- terarkdb-sys/build.rs
+++ terarkdb-sys/build.rs
@@ -1,2 +1,7 @@
+use std::env;
+
 fn main() {
+    println!("cargo::rerun-if-changed=terarkdb");
+    println!("cargo::rustc-link-search=native=terarkdb/output/lib");
+    println!("cargo::rustc-link-lib=static=terarkdb");
 }
