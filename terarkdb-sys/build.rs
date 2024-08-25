f2a6d7acc0128b4e52f392fb1441473b73fb2bec
Niklas Fiekas <niklas.fiekas@backscattering.de>
2024-08-25 17:26:03 +0200

    create cargo workspace

--- terarkdb-sys/build.rs
+++ terarkdb-sys/build.rs
@@ -1,8 +1,11 @@
 use std::{env, path::PathBuf};
 
 fn main() {
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let manifest_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+
     println!("cargo::rerun-if-changed=terarkdb");
-    println!("cargo::rustc-link-search=native=terarkdb/output/lib");
+    println!("cargo::rustc-link-search=native={}", manifest_dir.join("terarkdb").join("output").join("lib").display());
     println!("cargo::rustc-link-lib=static=terarkdb");
     println!("cargo::rustc-link-lib=static=terark-zip-r");
     println!("cargo::rustc-link-lib=static=boost_fiber");
@@ -17,8 +20,6 @@ fn main() {
     println!("cargo::rustc-link-lib=dylib=aio");
     println!("cargo::rustc-link-lib=dylib=gomp");
     println!("cargo::rustc-link-lib=dylib=stdc++");
-
-    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
     bindgen::builder()
         .layout_tests(false)
--- terarkdb-sys/src/lib.rs
+++ terarkdb-sys/src/lib.rs
@@ -1,4 +1,5 @@
 #![forbid(unsafe_op_in_unsafe_fn)]
 #![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
