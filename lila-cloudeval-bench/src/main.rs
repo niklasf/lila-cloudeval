2bde817490903761ec634319aa474aa89b1486ec
Niklas Fiekas <niklas.fiekas@backscattering.de>
2024-09-04 22:42:48 +0200

    show some basic stats immediately

--- lila-cloudeval-bench/src/main.rs
+++ lila-cloudeval-bench/src/main.rs
@@ -4,12 +4,13 @@ use std::{
     io,
     io::{BufRead as _, BufReader},
     path::PathBuf,
+    sync::atomic::{AtomicU64, Ordering},
     time::Instant,
 };
 
 use clap::Parser;
 use rayon::prelude::*;
-use reqwest::blocking::Client;
+use reqwest::{blocking::Client, StatusCode};
 
 #[derive(clap::Parser)]
 struct Opt {
@@ -33,22 +34,34 @@ fn main() -> Result<(), Box<dyn Error>> {
         .build()
         .expect("client");
 
+    let oks = AtomicU64::new(0);
+    let bad_requests = AtomicU64::new(0);
+    let others = AtomicU64::new(0);
+
     // With RAYON_NUM_THREADS
     fens.par_iter().for_each(|fen| {
         let before = Instant::now();
+
         let res = client
             .get(&opt.endpoint)
             .query(&[("fen", fen)])
             .send()
             .expect("send");
 
-        if res.status()
-            .error_for_status()
-            .expect("ok")
-            .text()
-            .expect("text");
+        (match res.status() {
+            StatusCode::OK => &oks,
+            StatusCode::BAD_REQUEST => &bad_requests,
+            _ => &others,
+        })
+        .fetch_add(1, Ordering::Relaxed);
+
+        let _ = res.text().expect("text");
         println!("{}\t{}", fen, before.elapsed().as_millis());
     });
+
+    eprintln!("ok: {}", oks.load(Ordering::Relaxed));
+    eprintln!("bad requests: {}", bad_requests.load(Ordering::Relaxed));
+    eprintln!("others: {}", others.load(Ordering::Relaxed));
 
     Ok(())
 }
