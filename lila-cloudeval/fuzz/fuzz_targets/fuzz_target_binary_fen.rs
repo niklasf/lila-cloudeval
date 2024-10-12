ad9956b874403c19d0d55242947e406c3da3ae47
Niklas Fiekas <niklas.fiekas@backscattering.de>
2024-10-12 17:44:08 +0200

    wip fuzz target

--- lila-cloudeval/fuzz/fuzz_targets/fuzz_target_binary_fen.rs
+++ lila-cloudeval/fuzz/fuzz_targets/fuzz_target_binary_fen.rs
@@ -1,6 +1,19 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
+use shakmaty::fen::Fen;
 
 fuzz_target!(|data: &[u8]| {
+    if let Ok(fen) Fen::parse_ascii(data) {
+        let original = VariantSetup {
+            setup: fen.into_setup(),
+            variant: Variant::Chess,
+        };
+
+        let mut buf = Vec::new();
+        original.write(&mut buf);
+
+        let roundtripped = VariantSetup::read(&mut buf[..]);
+        assert_eq!(original, roundtripped);
+    }
 });
